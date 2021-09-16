9bb1b7fec7fd86d713a1204ac56fd26b3993a6e3
Black Nguyen <blackng10.5.1997@gmail.com>
2021-09-16 18:10:06 +0700

    'update'

--- server/controllers/authCtrl.ts
+++ server/controllers/authCtrl.ts
@@ -8,9 +8,9 @@ import {
   generateRefreshToken,
 } from "../config/generateToken";
 import sendMail from "../config/sendMail";
-import { validPhone, validateEmail } from "../middleware/vaild";
+import { validateEmail, validPhone } from "../middleware/vaild";
 import { sendSms } from "../config/sendSMS";
-import { INewUser, IDecodedToken, IUser } from "../config/interface";
+import { IDecodedToken, IUser } from "../config/interface";
 
 const CLIENT_URL = `${process.env.BASE_URL}`;
 
@@ -30,86 +30,55 @@ const authCtrl = {
       const newUser = { name, account, password: passwordHash };
 
       const active_token = generateActiveToken({ newUser });
-      console.log(active_token);
+
       const url = `${CLIENT_URL}/active/${active_token}`;
 
       if (validateEmail(account)) {
-        sendMail(account, url, "Verify Email");
-
-        return res.json({
-          msg: "Register successfully.Please check email",
-        });
+        sendMail(account, url, "Verify your email address");
+        return res.json({ msg: "Success! Please check your email." });
       } else if (validPhone(account)) {
-        sendSms(account, url, "Verify Your Phone number");
-
-        return res.json({
-          msg: "Register successfully.Please check phone",
-        });
+        sendSms(account, url, "Verify your phone number");
+        return res.json({ msg: "Success! Please check phone." });
       }
     } catch (err: any) {
       return res.status(500).json({ msg: err.message });
     }
   },
-
   activeAccount: async (req: Request, res: Response) => {
     try {
       const { active_token } = req.body;
 
       const decoded = <IDecodedToken>(
-        jwt.verify(active_token, `${process.env.ACCESS_TOKEN_SECRET}`)
+        jwt.verify(active_token, `${process.env.ACTIVE_TOKEN_SECRET}`)
       );
 
       const { newUser } = decoded;
 
-      if (!newUser) return res.status(400).json({ msg: "Invalid authen" });
+      if (!newUser)
+        return res.status(400).json({ msg: "Invalid authentication." });
 
-      const user = new Users(newUser);
-      await user.save();
+      const user = await Users.findOne({ account: newUser.account });
+      if (user) return res.status(400).json({ msg: "Account already exists." });
 
-      res.json({ msg: "Account has been activated" });
+      const new_user = new Users(newUser);
+
+      await new_user.save();
+
+      res.json({ msg: "Account has been activated!" });
     } catch (err: any) {
-      let errMsg;
-
-      if (err.code === 11000) {
-        errMsg = Object.keys(err.keyValue)[0] + " already exists";
-      } else {
-        let name = Object.keys(err.errors)[0];
-        errMsg = err.errors[`${name}`].message;
-      }
-
-      return res.status(500).json({ msg: errMsg });
+      return res.status(500).json({ msg: err.message });
     }
   },
-
   login: async (req: Request, res: Response) => {
     try {
       const { account, password } = req.body;
 
       const user = await Users.findOne({ account });
+      if (!user)
+        return res.status(400).json({ msg: "This account does not exits." });
 
-      if (!user) {
-        return res.status(400).json({ msg: "This account does not exits" });
-      }
-
-      //if user exist
-      const isMatch = await bcrypt.compare(password, user.password);
-      if (!isMatch)
-        return res.status(400).json({ msg: "Password is incorrect." });
-
-      const access_token = generateAccessToken({ id: user._id });
-      const refresh_token = generateRefreshToken({ id: user._id });
-
-      res.cookie("refreshtoken", refresh_token, {
-        httpOnly: true,
-        path: `/api/refresh_token`,
-        maxAge: 30 * 24 * 60 * 60 * 1000, // 30days
-      });
-
-      res.json({
-        msg: "Login Success!",
-        access_token,
-        user: { ...user._doc, password: "" },
-      });
+      // if user exists
+      loginUser(user, password, res);
     } catch (err: any) {
       return res.status(500).json({ msg: err.message });
     }
@@ -144,6 +113,26 @@ const authCtrl = {
       return res.status(500).json({ msg: err.message });
     }
   },
+};
+
+const loginUser = async (user: IUser, password: string, res: Response) => {
+  const isMatch = await bcrypt.compare(password, user.password);
+  if (!isMatch) return res.status(400).json({ msg: "Password is incorrect." });
+
+  const access_token = generateAccessToken({ id: user._id });
+  const refresh_token = generateRefreshToken({ id: user._id });
+
+  res.cookie("refreshtoken", refresh_token, {
+    httpOnly: true,
+    path: `/api/refresh_token`,
+    maxAge: 30 * 24 * 60 * 60 * 1000, // 30days
+  });
+
+  res.json({
+    msg: "Login Success!",
+    access_token,
+    user: { ...user._doc, password: "" },
+  });
 };
 
 export default authCtrl;
