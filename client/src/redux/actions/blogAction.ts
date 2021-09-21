c535b6062a52a59a14417b336a70aded3957850f
Black Nguyen <blackng10.5.1997@gmail.com>
2021-09-21 09:28:57 +0700

    'update'

--- client/src/redux/actions/blogAction.ts
+++ client/src/redux/actions/blogAction.ts
@@ -10,6 +10,8 @@ import {
   IGetHomeBlogsType,
   GET_BLOGS_CATEGORY_ID,
   IGetBlogsCategoryType,
+  GET_BLOGS_USER_ID,
+  IGetBlogsUserType,
 } from "../types/blogType";
 
 export const createBlog =
@@ -63,10 +65,32 @@ export const getBlogsByCategoryId =
 
       dispatch({ type: ALERT, payload: { loading: true } });
 
-      const res = await getAPI(`blogs/${id}${value}&limit=${limit}`);
+      const res = await getAPI(`blogs/category/${id}${value}&limit=${limit}`);
 
       dispatch({
         type: GET_BLOGS_CATEGORY_ID,
+        payload: { ...res.data, id, search },
+      });
+
+      dispatch({ type: ALERT, payload: { loading: false } });
+    } catch (err: any) {
+      dispatch({ type: ALERT, payload: { errors: err.response.data.msg } });
+    }
+  };
+
+export const getBlogsByUserId =
+  (id: string, search: string) =>
+  async (dispatch: Dispatch<IAlertType | IGetBlogsUserType>) => {
+    try {
+      let limit = 3;
+      let value = search ? search : `?page=${1}`;
+
+      dispatch({ type: ALERT, payload: { loading: true } });
+
+      const res = await getAPI(`blogs/user/${id}${value}&limit=${limit}`);
+
+      dispatch({
+        type: GET_BLOGS_USER_ID,
         payload: { ...res.data, id, search },
       });
 
