89fe5d99257cf710b2dd52d21694609c1d262bf2
Black Nguyen <blackng10.5.1997@gmail.com>
2021-09-25 14:05:30 +0700

    'update'

--- server/index.ts
+++ server/index.ts
@@ -9,7 +9,7 @@ import routes from "./routes/index";
 import { createServer } from "http";
 import { Server, Socket } from "socket.io";
 
-//Middleware
+// Middleware
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -17,7 +17,7 @@ app.use(cors());
 app.use(morgan("dev"));
 app.use(cookieParser());
 
-// Socket .io
+// Socket.io
 const http = createServer(app);
 export const io = new Server(http);
 import { SocketServer } from "./config/socket";
@@ -27,16 +27,12 @@ io.on("connection", (socket: Socket) => {
 });
 
 // Routes
-app.use("/api", routes.authRouter);
-app.use("/api", routes.userRouter);
-app.use("/api", routes.categoryRouter);
-app.use("/api", routes.blogRouter);
-app.use("/api", routes.commentRouter);
+app.use("/api", routes);
 
 // Database
 import "./config/database";
 
-// Server Listen
+// server listenning
 const PORT = process.env.PORT || 5000;
 http.listen(PORT, () => {
   console.log("Server is running on port", PORT);
--- server/routes/blogRouter.ts
+++ server/routes/blogRouter.ts
@@ -1,21 +1,24 @@
-import express from "express";
-import blogCtrl from "../controllers/blogCtrl";
-import auth from "../middleware/auth";
+import express from 'express'
+import blogCtrl from '../controllers/blogCtrl'
+import auth from '../middleware/auth'
 
-const router = express.Router();
+const router = express.Router()
 
-router.post("/blog", auth, blogCtrl.createBlog);
 
-router.get("/home/blogs", blogCtrl.getHomeBlogs);
+router.post('/blog', auth, blogCtrl.createBlog)
 
-router.get("/blogs/category/:id", blogCtrl.getBlogsByCategory);
+router.get('/home/blogs', blogCtrl.getHomeBlogs)
 
-router.get("/blogs/user/:id", blogCtrl.getBlogsByUser);
+router.get('/blogs/category/:id', blogCtrl.getBlogsByCategory)
 
-router
-  .route("/blog/:id")
+router.get('/blogs/user/:id', blogCtrl.getBlogsByUser)
+
+router.route('/blog/:id')
   .get(blogCtrl.getBlog)
   .put(auth, blogCtrl.updateBlog)
-  .delete(auth, blogCtrl.deleteBlog);
+  .delete(auth, blogCtrl.deleteBlog)
+
+router.get('/search/blogs', blogCtrl.searchBlogs)
+
 
 export default router;
